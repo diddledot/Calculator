dedf11245f630d68c8cf2f0c92dfd42a8159b62e
diddledot <diddledot@users.noreply.github.com>
2014-10-20 17:49:04 -0600

    Update calculator.php

--- calculator.php
+++ calculator.php
@@ -6,79 +6,80 @@
 <style type="text/css">
 .carpet-choice > input{ /* HIDE RADIO */
 
-  position: absolute;
 }
-
 .carpet-choice > label{ /* LABELS */
-  position: absolute;
-  color: white;
+color: white;
 }
 
 .carpet-choice > input + img{ /* IMAGE STYLES */
-  cursor:pointer;
-  border:3px solid transparent;
+cursor:pointer;
+border:3px solid transparent;
 }
 .carpet-choice > input:checked + img{ /* (RADIO CHECKED) IMAGE STYLES */
-  border:2px solid #f00;
+border:2px solid #f00;
 }
-
-.carpet-choice input[type="radio"]:checked + img {
-    border: 3px solid red;
+.carpet-choice input[type="radio"]:checked + label img {
+border: 3px solid red;
 }
 </style>
 </head>
-
 <body>
 <h1>Carpet Calculator</h1>
-
 <form id="multiPhase" onSubmit="return false">
-<h3>Choose Carpet:</h3>
-  <div class="carpet-choice">
-    <label for="brown">Brown Carpet $2.99</label>
-    <input id="brown" type="radio" name="carpet" value="2.99" />
-    <img src="brown.jpg" alt=""/>
-  </div>
-  <div class="carpet-choice">
-  <label for="darkgrey">Dark Grey Carpet $3.99</label>
-    <input id="darkgrey" type="radio" name="carpet" value="3.99" />
-    <img src="darkgrey.jpg" alt=""/>
-  </div>
-  <div class="carpet-choice">
-    <label for="grey">Light Grey Carpet $1.99</label>
-    <input id="grey" type="radio" name="carpet" value="1.99" />
-    <img src="grey.jpg" alt=""/>
-  </div>
-  <div class="carpet-choice">
-    <label for="purple">Purple Carpet $4.99</label>
-    <input id="purple" type="radio" name="carpet" value="4.99" />
-    <img src="purple.jpg" alt=""/>
-  </div>
-  <div class="carpet-choice">
-   <label for="red">Red Carpet $3.99</label>
-    <input id="red" type="radio" name="carpet" value="3.99" />
-    <img src="red.jpg" alt=""/>
-  </div>
-  <div class="carpet-choice">
-    <label for="white">White Carpet $2.50</label>
-    <input id="white" type="radio" name="carpet" value="2.50" />
-    <img src="white.jpg" alt=""/>
-  </div>
-       
-<label for="rooms">How many rooms will be carpeted?</label>
-<input id="rooms" name="rooms" type="number" value="1" min="1" max="10"/><br/>
-  
-<label for="stairs">Carpeting a stairway?</label>
-<select id="stairs" name="stairs">
-  <option value="No">No</option>
-  <option value="Yes">Yes</option>
-</select><br/>          
-<input type="submit" name="submit1" value="Continue">
-</form>   
-
+	<h3>Choose Carpet:</h3>
+	<div class="carpet-choice">
+		<input id="brown" type="radio" name="carpet" value="2.99" />
+		<label for="brown">
+			Brown Carpet $2.99
+			<img src="brown.jpg" alt=""/>
+		</label>
+	</div>
+	<div class="carpet-choice">
+		<input id="darkgrey" type="radio" name="carpet" value="3.99" />
+		<label for="darkgrey">
+			Dark Grey Carpet $3.99
+			<img src="darkgrey.jpg" alt=""/>
+		</label>	
+	</div>	
+	<div class="carpet-choice">
+		<input id="grey" type="radio" name="carpet" value="1.99" />
+		<label for="grey">
+			Light Grey Carpet $1.99
+			<img src="grey.jpg" alt=""/>
+		</label>	
+	</div>
+	<div class="carpet-choice">
+		<input id="purple" type="radio" name="carpet" value="4.99" />
+		<label for="purple">
+			Purple Carpet $4.99
+			<img src="purple.jpg" alt=""/>
+		</label>
+	</div>
+	<div class="carpet-choice">
+		<input id="red" type="radio" name="carpet" value="3.99" />
+		<label for="red">
+			Red Carpet $3.99
+			<img src="red.jpg" alt=""/>
+		</label>
+	</div>
+	<div class="carpet-choice">
+		<input id="white" type="radio" name="carpet" value="2.50" />
+		<label for="white">
+			White Carpet $2.50
+			<img src="white.jpg" alt=""/>
+		</label>
+	</div>
+	
+	<label for="rooms">How many rooms will be carpeted?</label>
+	<input id="rooms" name="rooms" type="number" value="1" min="1" max="10"/><br/>
+	<label for="stairs">Carpeting a stairway?</label>
+	<select id="stairs" name="stairs">
+		<option>No</option>
+		<option>Yes</option>
+	</select><br/>
+	<input type="submit" name="submit1" value="Continue">
+</form>
 <?php
- 
-?>   
-
-
+?>
 </body>
 </html>
